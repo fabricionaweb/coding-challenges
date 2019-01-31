739ed72897e869c98c40458a89dc2d7ff569c58d
Fabricio Augusto <fabricionaweb@gmail.com>
2019-01-31 23:53:07 +0000

    Solve the javascript challenge

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -1,8 +1,44 @@
+const MIN_WIDTH = 2;
+const CHARACTER = '@';
+const SPACE = ' ';
+const EOL = '\n';
+
 /**
  * Returns the adidas three stripes logo using `@` characters.
  * @param {number} width - Width of a stripe.
+ * @param {number} [stripes] - Number of stripes.
  * @returns {string} adidas logo.
  */
-module.exports = function(width) {
-  return `${ width }`;
-};
+function createLogo(width, stripes = 3) {
+  if (width < MIN_WIDTH) {
+    throw new Error('Error, minimun width is 2');
+  }
+
+  const height = Math.round(Math.sqrt(width));
+  const distance = SPACE.repeat(height);
+  const body = CHARACTER.repeat(width);
+  const matrix = new Array(height * stripes).fill([ body ]);
+
+  const fixStripesHeight = (row, index) => {
+    const cols = Math.floor(index / height);
+    const fill = new Array(cols).fill(body);
+
+    return row.concat(fill);
+  };
+
+  const drawColumns = (row, index) => {
+    const missCols = stripes - row.length;
+    const emptyCols = SPACE.repeat(width * missCols);
+    const fixSpaces = SPACE.repeat(index % height);
+    const content = row.join(distance);
+
+    return emptyCols + fixSpaces + content;
+  };
+
+  return matrix
+    .map(fixStripesHeight)
+    .map(drawColumns)
+    .join(EOL);
+}
+
+module.exports = createLogo;

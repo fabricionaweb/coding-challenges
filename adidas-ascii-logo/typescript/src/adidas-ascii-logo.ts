924efa1bac21f92106ec3f37960ef2cb9dbd66c8
Fabricio Augusto <fabricionaweb@gmail.com>
2019-02-01 00:53:29 +0000

    Solve the typescript challenge (similar as javascript)

--- adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
+++ adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
@@ -1,8 +1,45 @@
+const MIN_WIDTH = 2;
+const CHARACTER = '@';
+const SPACE = ' ';
+const EOL = '\n';
+
+type Row = Array<string>;
+type Matrix = Array<Row>;
+
 /**
  * Returns the adidas three stripes logo using `@` characters.
  * @param width - Width of a stripe.
+ * @param [stripes=3] - Number of stripes.
  * @returns adidas logo.
  */
-export function createAdidasAsciiLogo(width: number): string {
-  return `${ width }`;
+export function createAdidasAsciiLogo(width: number, stripes = 3): string {
+  if (width < MIN_WIDTH) {
+    throw new Error('Error, minimun width is 2');
+  }
+
+  const height = Math.round(Math.sqrt(width));
+  const distance = SPACE.repeat(height);
+  const body = CHARACTER.repeat(width);
+  const matrix: Matrix = new Array(height * stripes).fill([ body ]);
+
+  const fixStripesHeight = (row: Row, index: number): Row => {
+    const cols = Math.floor(index / height);
+    const fill: Row = new Array(cols).fill(body);
+
+    return row.concat(fill);
+  };
+
+  const drawColumns = (row: Row, index: number): string => {
+    const missCols = stripes - row.length;
+    const emptyCols = SPACE.repeat(width * missCols);
+    const fixSpaces = SPACE.repeat(index % height);
+    const content = row.join(distance);
+
+    return emptyCols + fixSpaces + content;
+  };
+
+  return matrix
+    .map(fixStripesHeight)
+    .map(drawColumns)
+    .join(EOL);
 }
